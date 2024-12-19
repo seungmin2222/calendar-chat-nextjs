9de8d184f1d4eb9dbb2c9dba5678b5718f0bf52a
SeungMin Lee <wooju2602@gmail.com>
2024-12-20 06:08:03 +0900

    ✨ Feat: 채팅 폼 토글 버튼 구현 및 chat.tsx 컴포넌트 연결 완료

--- src/app/@chat/page.tsx
+++ src/app/@chat/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+import ChatForm from './components/ChatForm';
+
 export default function Chat() {
+  const [isChatFormVisible, setIsChatFormVisible] = useState(false);
+
+  const toggleChatForm = () => {
+    setIsChatFormVisible((prevState) => !prevState);
+  };
+
   return (
-    <div className="absolute bottom-10 right-10 rounded-lg bg-gray-800 px-4 py-2 text-white shadow-lg">
-      Chat
+    <div>
+      <button
+        onClick={toggleChatForm}
+        className="absolute bottom-10 right-10 rounded-lg bg-blue-500 px-4 py-2 text-white shadow-lg duration-[400ms] hover:bg-blue-600"
+      >
+        Chat 💬
+      </button>
+      {isChatFormVisible && <ChatForm />}
     </div>
-  )
+  );
 }
