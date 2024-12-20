c9103178415832f7ecea1d23e55878c05de70fd8
SeungMin Lee <wooju2602@gmail.com>
2024-12-20 15:57:05 +0900

    ✨ Feat: CalendarHeader에 모든 예약 일정을 볼 수 있는 버튼 추가

--- src/app/(calendar)/components/CalendarHeader.tsx
+++ src/app/(calendar)/components/CalendarHeader.tsx
@@ -12,12 +12,15 @@ export default function CalendarHeader({
   onNextMonth,
 }: CalendarHeaderProps) {
   return (
-    <header className="flex h-11 min-h-10 items-center justify-center gap-5 rounded-md bg-white text-5xl text-gray-700">
+    <header className="relative flex h-11 min-h-10 items-center justify-center gap-5 rounded-md bg-white text-5xl text-gray-700">
       <button onClick={onPrevMonth}>{'<'}</button>
       <span className="w-44 text-center text-3xl font-semibold">
         {year}년 {month}월
       </span>
       <button onClick={onNextMonth}>{'>'}</button>
+      <button className="absolute right-4 rounded-lg bg-blue-600 px-4 py-1 text-base font-medium text-white shadow-lg transition duration-300 hover:bg-blue-700">
+        일정 보기
+      </button>
     </header>
   );
 }
